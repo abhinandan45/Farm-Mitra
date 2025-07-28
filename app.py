21f953cb6818a4d28a4ce5304d16047044a89938
Abhinandan <abhinandansendhav540@gmail.com>
2025-07-28 13:33:13 +0530

    change port type

--- app.py
+++ app.py
@@ -446,6 +446,10 @@ def chat():
         return jsonify({"response": f"Sorry, an internal error occurred: {e}. Please try again later."}), 500
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     
-    app.run(debug=True)
+#     app.run(debug=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host='0.0.0.0', port=port)
