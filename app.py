c8922f68fec187fb8e4a94b30c51dc1403061516
Abhinandan <abhinandansendhav540@gmail.com>
2025-07-28 14:46:40 +0530

    add render.yaml file

--- app.py
+++ app.py
@@ -449,6 +449,6 @@ def chat():
     
 #     app.run(debug=True)
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000)) 
+    app.run(host="0.0.0.0", port=port)
